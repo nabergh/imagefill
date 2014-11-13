966ce47c9a436c77d753b4bd702679787f4605a2
Nicholas Bergh <nicholasabergh@gmail.com>
2014-11-12 20:21:32 -0500

    changed command line params to take in square size

--- imagefill.cpp
+++ imagefill.cpp
@@ -5,14 +5,16 @@ using namespace cimg_library;
 CImg<unsigned char> source;
 CImg<unsigned char> matte;
 CImg<unsigned char> result;
+int squaresize;
 
 
 int main(int argc, char *argv[]) {
-	if (argc > 3) {
+	if (argc > 2) {
 		source = CImg<unsigned char>(argv[1]);
+		squaresize = atoi(argv[2]);
 		source.display();
 	} else {
-		printf("You must parameters like so: /imagequilt [in file] [matte file] [out file]\n");
+		printf("You must parameters like so: /imagequilt [in file] [square size]\n");
 		return 0;
 	}
 
