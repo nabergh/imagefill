710bfbdc19ed0c38f982797609057f3ce779e736
Nicholas Bergh <nicholasabergh@gmail.com>
2014-11-12 20:40:56 -0500

    added fill square

--- imagefill.cpp
+++ imagefill.cpp
@@ -1,17 +1,32 @@
 #include "CImg.h"
+#include "math.h"
 using namespace cimg_library;
 
 
 CImg<unsigned char> source;
 CImg<unsigned char> matte;
 CImg<unsigned char> result;
+int squareleft;
+int squareright;
+int squaretop;
+int squarebottom;
 int squaresize;
+
+float confidence() {
+	return 0;
+}
 
 
 int main(int argc, char *argv[]) {
 	if (argc > 3) {
 		source = CImg<unsigned char>(argv[1]);
 		squaresize = atoi(argv[2]);
+		srand (time(NULL));
+		squareleft = rand() % (source.width() - squaresize);
+		squaretop = rand() % (source.height() - squaresize);
+		squareright = squareleft + squaresize;
+		squarebottom = squaretop + squaresize;
+
 		source.display();
 	} else {
 		printf("You must parameters like so: /imagequilt [in file] [square size]\n");
